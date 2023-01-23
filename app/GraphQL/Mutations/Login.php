e58b15cfa8f6f4c907968b11ba84b23ebc308a32
Ndifreke Alexander <alexanderndifreke4@gmail.com>
2023-01-23 22:47:35 +0100

    Resolved token and auth user function

---
+++ app/GraphQL/Mutations/Login.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+final class Login
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $user = User::where('email', $args['email'])->first();
+
+        if (! $user || ! Hash::check($args['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+    return $user->createToken($args['device'])->plainTextToken;
+    }
+}
